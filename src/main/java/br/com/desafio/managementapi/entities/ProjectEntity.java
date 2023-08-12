fde488ab1fe8cf758f5184f25511964965abef32
Filipe Chiconello <filipe360@gmail.com>
2023-08-12 19:02:54 -0300

    :sparkles: feat: implementating project entity

--- src/main/java/br/com/desafio/managementapi/entities/ProjectEntity.java
+++ src/main/java/br/com/desafio/managementapi/entities/ProjectEntity.java
@@ -1,4 +1,25 @@
 package br.com.desafio.managementapi.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "projects")
 public class ProjectEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private LocalDateTime createdAt;
 }
