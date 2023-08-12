3670fbb8e061dc2eaddb192c661db91cd0453862
Filipe Chiconello <filipe360@gmail.com>
2023-08-12 18:15:58 -0300

    first commit

---
+++ src/main/java/br/com/desafio/managementapi/ManagementApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.desafio.managementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementApiApplication.class, args);
+	}
+}
