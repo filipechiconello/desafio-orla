f5cb1103db80751daf2c33da94557e171666c8f2
Filipe Chiconello <filipe360@gmail.com>
2023-08-12 19:05:34 -0300

    :sparkles: feat: implementating requests and responses from project

---
+++ src/main/java/br/com/desafio/managementapi/dtos/requests/ProjectRequestDTO.java
@@ -0,0 +1,9 @@
+package br.com.desafio.managementapi.dtos.requests;
+
+import java.time.LocalDateTime;
+
+public class ProjectRequestDTO {
+
+    private String name;
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/br/com/desafio/managementapi/dtos/responses/ProjectResponseDTO.java
@@ -0,0 +1,11 @@
+package br.com.desafio.managementapi.dtos.responses;
+
+import java.time.LocalDateTime;
+
+
+public class ProjectResponseDTO {
+
+    private Long id;
+    private String name;
+    private LocalDateTime createdAt;
+}
